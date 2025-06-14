5d8cedd4958feb6ddbada8ce66ff744402f5c611
zaarabuy <zaarabuyghimire@gmail.com>
2025-06-14 02:47:50 +0000

    Changes made in mysql script

--- mysql.sql
+++ mysql.sql
@@ -10,5 +10,6 @@ location VARCHAR(20));
 
 INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
 INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('2','Yubaraj','Ghimire','student','local');
 SELECT * FROM employee;
 
